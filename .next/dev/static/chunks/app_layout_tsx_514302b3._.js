f4aa5fae73726f6fb27e967679e010d2e9b99196
Waqar Niyazi <niyazi.waqar007@gmail.com>
2025-12-15 13:48:39 +0530

    added faviconsand logo

--- .next/dev/static/chunks/app_layout_tsx_514302b3._.js
+++ .next/dev/static/chunks/app_layout_tsx_514302b3._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/node_modules__pnpm_46615523._.js",
+  "static/chunks/node_modules__pnpm_8253ffb9._.js",
   "static/chunks/_cb7342bf._.js"
 ],
     source: "dynamic"
--- .next/dev/static/chunks/e559b_next_dist_client_components_builtin_global-error_514302b3.js
+++ .next/dev/static/chunks/e559b_next_dist_client_components_builtin_global-error_514302b3.js
@@ -1,7 +1,5 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/e559b_next_dist_03f6cd6c._.js"
-],
+    chunks: [],
     source: "dynamic"
 });
